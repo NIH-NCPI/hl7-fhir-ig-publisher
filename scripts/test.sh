4c8fa5bd8dadffada5376ecea11b6bc3cd25aa1f
Natasha Singh <dukedesi22@gmail.com>
2019-12-03 20:29:54 -0500

    :white_check_mark: Add test script

---
+++ scripts/test.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+# Integration test to make sure basic implementation guide publishing works
+
+set -eo pipefail
+
+echo "************** Start integration tests **************"
+
+ig_site_dir=${1:-"$(pwd)/test/ig-site"}
+
+# Check if user specified a location for ig site
+if [[ -z $1 ]]; then
+    echo "Using default location for implementation guide: $ig_site_dir"
+else
+    echo "Using user specified location for implementation guide: $ig_site_dir"
+fi
+
+# Run ig-publisher on test ig
+docker run --rm -it -v "$ig_site_dir":/data kidsfirstdrc/fhir-ig-publisher -ig /data/ig.ini -tx n/a
+
+echo "************** End integration tests **************"
